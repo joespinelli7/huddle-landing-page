44a8765d2815319bd5cf361e5278118282db8b73
github username <josephspinelli@yahoo.com>
2021-05-03 15:33:55 -0500

    gulp: Update gulp syntax and set default command

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 'use strict';
 
-var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
+const { src, dest, parallel, series } = require('gulp');
+const concatCss = require('gulp-concat-css');
 
-gulp.task('css', function() {
-  return gulp.src('css/*/*.css')
-    .pipe(concatCss('css/styles.css'))
-    .pipe(gulp.dest('.'));
-});
+function generateCSS(cb) {
+  src('./css/*/*.css')
+    .pipe(concatCss('./css/styles.css'))
+    .pipe(dest('.'));
+  cb();
+}
+
+// this makes default gulp command, runs on just 'gulp'. Can chain multiple gulp tasks to run by default.
+// Check 'updated gulp syntax' article below.
+exports.default = generateCSS;
+
+// this runs on 'gulp css'
+// exports.css = generateCSS;
 
 // article followed:
 // https://travishorn.com/integrating-multiple-stylesheets-in-a-single-site-e720256edcee
+
+// updated gulp syntax:
+// https://semaphoreci.com/community/tutorials/getting-started-with-gulp-js
